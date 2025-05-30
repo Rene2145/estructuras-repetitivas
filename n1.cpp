447efbb6767c8c52902a85ad6b33d866c7572c12
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-29 23:51:13 -0500

    Actualiza ejercicio 1 de estructuras repetitivas: mejoramiento de interfaz

--- n1.cpp
+++ n1.cpp
@@ -3,13 +3,12 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "¿Cuantos multiplos de 7 te gustaria ver? : ";
+    cout << "¿Cuantos multiplos de 7 te gustaria ver? : "<<endl;
     cin >> n;
-
-    for(int i = 1; i <= n; i++) {
+ 	cout << "Excelente, estos serian los multiplos de 7: "<<endl;
+    for(int i = 1; i <= n; i=i+1) {
         cout << 7 * i << " ";
     }
-    
     return 0;
 }
 
