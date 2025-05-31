9c8b3beb524e9650a7b932a072d7d2147d79aacb
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-30 22:52:05 -0500

    Actualiza ejercicio 2 de estructuras repetitivas: asegurar solo numeros enteros

--- n2.cpp
+++ n2.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int numero;
-    cout << "Por favor escribe cualquier numero: ";
-    cin >> numero;
+	double numero_decimal;
+    int numero_positivo;
+    cout << "Por favor escribe un numero positivo: "<<endl;
+    cin >> numero_decimal;
+    numero_positivo = (int)numero_decimal;
     
-    while(numero >= 0) {
-        cout << "Escribe otro numero.: ";
-        cin >> numero;
+    while(numero_positivo >= 0) {
+        cout << "Escribe otro numero"<<endl;
+        cin >> numero_decimal;
+        numero_positivo=(int)numero_decimal;
     }
-    
     cout << "Fin de la captura" << endl;
     
     return 0;
