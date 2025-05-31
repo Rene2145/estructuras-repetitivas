adbeef125bc065aa1e49e98a2d575d58522ce7fa
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-05-30 23:18:09 -0500

    Actualiza ejercicio 3 de estructuras repetititivas: mejoramiento de interfaz

--- n3.cpp
+++ n3.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 int main() {
     int n, suma = 0;
-    cout << "Calcularemos la suma de los numeros desde 1 hasta el numero que escribas "<< endl;
-    cout << "Escribe un numero : ";
+    cout << "Este programa te ayudara a sumar los numeros naturales desde el 1 hasta cualquier numero que escribas"<< endl;
     cin >> n;
-
-    for(int i = 1; i <= n; i++) {
-        suma += i;
+    for(int i = 1; i <= n; i=i+1) {
+        suma=suma+i;
     }
 
-    cout << "El resultado de la suma es: " << suma << endl;
+    cout << "El resultado de la suma es: "<< endl;
+    cout<<suma;
     return 0;
 }
 
