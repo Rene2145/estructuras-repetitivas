e0fe0efaffdc7098dc638558e6c2507e5f71eadd
Rene David Charaña Jalire <darkreingreso@gmail.com>
2025-06-02 20:56:22 -0500

    Actualiza ejercicio 6 de estructuras repetitivas: Mejoramiento de interfaz para el usuario

--- n6.cpp
+++ n6.cpp
@@ -3,21 +3,24 @@ using namespace std;
 
 int main() {
     int n, numero, suma = 0, contador = 0;
-    cout << "¿Cuantos numeros quieres ingresar? ";
+    cout << "<<< PROMEDIO DE NUMEROS NO NEGATIVOS >>> "<<endl<<" "<<endl;
+    cout << "¿Cuantos numeros quieres ingresar? "<<endl;
     cin >> n;
 
     for(int i = 0; i < n; i++) {
-        cout << "Escribe un numero: ";
+        cout << "Escribe un numero: "<<endl;
         cin >> numero;
 
         if(numero >= 0) {
-            suma += numero;
-            contador++;
-        }
+            suma=suma+numero;
+            contador=contador+1;
+        }else{
+			cout<<"¡Numero negativo no considerado!"<<endl;
+		}
     }
 
     if(contador > 0) {
-        cout << "El promedio de los numeros no negativos de la lista es: " << (float)suma / contador << endl;
+        cout << "El promedio de los numeros no negativos de la lista es: " <<endl<< (float)suma / contador << endl;
     } else {
         cout << "No se ingresaron numeros no negativos." << endl;
     }
